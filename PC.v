5554740231424fb4e8b6eb0d246bb495051c7b9a
Chiang, Yi-Yo@home <b03902082@ntu.edu.tw>
2015-12-02 20:53:17 +0800

    eliminate redundancy code

--- PC.v
+++ PC.v
@@ -21,10 +21,7 @@ always@(posedge clk_i or negedge start_i) begin
         pc_o <= 32'b0;
     end
     else begin
-        if(start_i)
-            pc_o <= pc_i;
-        else
-            pc_o <= pc_o;
+        pc_o <= pc_i;
     end
 end
 
