df9173c936ae1d0daf9a36c1a0d61ba96644bf94
pzread <netfirewall@gmail.com>
2015-12-07 12:38:24 +0800

    Make no sense

--- Data_Memory.v
+++ Data_Memory.v
@@ -20,12 +20,14 @@ reg		[31:0]		data_o;
 reg     [7:0]     memory  [0:31];
 
 
-always@(MemWrite_i or MemRead_i or addr_i or data_i) begin
+always@(posedge Clock_i) begin
 	if(MemWrite_i) begin
-		{memory[addr_i + 3], memory[addr_i + 2], memory[addr_i + 1], memory[addr_i]} = data_i;
+		{memory[addr_i + 3], memory[addr_i + 2], memory[addr_i + 1], memory[addr_i]} <= data_i;
 	end
-	else if(MemRead_i) begin
-		data_o = {memory[addr_i + 3], memory[addr_i + 2], memory[addr_i + 1], memory[addr_i]};
+end
+always@(negedge Clock_i) begin
+	if(MemRead_i) begin
+		data_o <= {memory[addr_i + 3], memory[addr_i + 2], memory[addr_i + 1], memory[addr_i]};
 	end
 end
 
